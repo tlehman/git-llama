39150f6b5413104247ea1b6254c472eb6cd32ece
Tobi Lehman <mail@tobilehman.com>
2025-02-24 18:07:59 -0800

    Add usage() to main()

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/ollama/ollama/api"
+)
+
+func usage() {
+	fmt.Printf("Usage:\n  git-llama [your prompt, delimited by quotes]\n")
+}
+
+func main() {
+	// check if a single prompt is passed in
+	if len(os.Args) != 2 {
+		usage()
+	}
+
+	// check if ollama is running
+	_, err := api.ClientFromEnvironment()
+	if err != nil {
+		fmt.Sprintf("err: %s", err)
+		os.Exit(1)
+	}
+
+	// the first argument is assumed to be the prompt input
 }
